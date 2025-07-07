3ff655625bb506ddc0bdca0f52b9749534a0c175
reiabaid <reiabaid330@gmail.com>
2025-07-07 21:13:40 +0530

    Add files via upload

---
+++ day01-hello-world/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '20vh' }}>
+      <h1>Hello, Reia!</h1>
+      <p>Day 1 of React: Project is up and running.</p>
+    </div>
+  );
+}
+
+export default App;
